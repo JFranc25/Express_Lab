42f360cf11524d79a0e9013a16a476de6300477c
jfranc25 <josh.francois04@gmail.com>
2025-11-10 14:58:47 -0500

    added post and new posts pages

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express =
  require (`express`);
+ const userRouter = 
+ require('./routes/users');
+ const postRouter =
+ require(`./routes/posts`);
 const app = express(); //calling this function sets up a server
 app.set('view engine', 'ejs');
-
+app.use('/users', userRouter);
+app.use(`/posts`, postRouter);
 app.get('/', (req,res)=>{
     console.log('Here');
-    res.render("index");
+    res.render("index", {user: "Josh!",});
 }); // this function wil run when soemone goes to root fl
+app.get('/users', (req, res)=> {
+    res.send('User List');
+});
+app.get('/users/new', (req, res)=>{
+    res.send('New User Form')
+});
+app.get(`/posts`, (req, res)=> {
+    res.send(`Post List`);
+});
+app.get(`/posts/new`, (req, res)=>(
+    res.send(`Create a New Post`)
+));
+
 
 
 app.listen(3030);
